e27c81b21b05b34a338dcfb929db0f12aaf99d7c
shohsultoncode <shohsulton.3039@gmail.com>
2023-06-21 15:29:53 +0500

    .

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get('/')
+  ///zzzzzzfgvcvbn 
   getHello(): string {
     return this.appService.getHello();
   }
